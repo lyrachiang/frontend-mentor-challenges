5403c016b616164b71f30f4f703b7c13e43daeaf
Lyra Chiang <fullmoon1711@gmail.com>
2026-04-30 19:09:15 +0800

    Challenge-02: Improve HTML structure

--- 02-product-preview-card-component/src/components/App/App.tsx
+++ 02-product-preview-card-component/src/components/App/App.tsx
@@ -2,17 +2,19 @@ import classNames from 'classnames/bind';
 
 import styles from './App.module.scss';
 
-import Content from '@/components/Content';
+import Product from '@/components/Product';
 import Footer from '@/components/Footer';
 
 const cx = classNames.bind(styles);
 
 const App = () => {
   return (
-    <main className={cx('app-container')}>
-      <Content />
+    <div className={cx('app-container')}>
+      <main className={cx('main-container')}>
+        <Product />
+      </main>
       <Footer />
-    </main>
+    </div>
   );
 };
 
