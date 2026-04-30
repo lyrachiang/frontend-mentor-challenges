dac4e3e36774e0f1b8d5d5abc85b8bc3c0656bf0
Lyra Chiang <fullmoon1711@gmail.com>
2026-04-30 19:21:10 +0800

    Challenge-01: Improve HTML structure

--- 01-3-column-preview-card-component/src/components/App/App.tsx
+++ 01-3-column-preview-card-component/src/components/App/App.tsx
@@ -2,17 +2,44 @@ import classNames from 'classnames/bind';
 
 import styles from './App.module.scss';
 
-import Content from '@/components/Content';
+import Card from '@/components/Card';
 import Footer from '@/components/Footer';
 
 const cx = classNames.bind(styles);
 
+const carData = [
+  {
+    id: 'sedans',
+    title: 'Sedans',
+    content: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
+  },
+  {
+    id: 'suvs',
+    title: 'SUVs',
+    content: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
+  },
+  {
+    id: 'luxury',
+    title: 'Luxury',
+    content: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
+  }
+] as const;
+
 const App = () => {
   return (
-    <main className={cx('app-container')}>
-      <Content />
+    <div className={cx('app-container')}>
+      <main className={cx('main-container')}>
+        {carData.map((item) => {
+          return (
+            <Card
+              key={item?.id}
+              item={item}
+            />
+          );
+        })}
+      </main>
       <Footer />
-    </main>
+    </div>
   );
 };
 
